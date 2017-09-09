3744d0f07141fe0d85465e826e04f6b7cb083d36
JayCay <31072099+JayCay@users.noreply.github.com>
2017-09-09 22:58:32 +0800

    window and cleaning #include
    - removed <cstdlib> & <ctime>
    - added variable for width and height for convenience

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 
 #define FPS 60
 #define CSIZE 6
@@ -24,7 +22,9 @@ int colorcycle(int count)
 int main( void )
 {
 	srand( time( NULL ) );
-    sf::RenderWindow window( sf::VideoMode( 480, 320 ), "test" );
+	int width = 800;
+	int height = 720;
+    sf::RenderWindow window( sf::VideoMode( width, height ), "test" );
 	sf::Color color[CSIZE];
 	color[0] = sf::Color::Red;
 	color[1] = sf::Color::Green;
@@ -41,8 +41,8 @@ int main( void )
 	for (int i = 0; i < CirSIZE; i++)
 	{
 		curr = colorcycle(i);
-		posw = rand() % 480 + 1;
-		posh = rand() % 320 + 1;
+		posw = rand() % width + 1;
+		posh = rand() % height + 1;
 		circ[i].setRadius( 30.f );
 		circ[i].setPosition(posw, posh);
 		circ[i].setFillColor( color[curr] );
@@ -51,8 +51,8 @@ int main( void )
 	for (int i = 0; i < RecSIZE; i++)
 	{
 		curr = colorcycle(i);
-		posw = rand() % 480 + 1;
-		posh = rand() % 320 + 1;
+		posw = rand() % width + 1;
+		posh = rand() % height + 1;
 		rect[i].setSize( sf::Vector2f( 50.f, 50.f ) );
 		rect[i].setPosition(posw, posh);
 		rect[i].setFillColor( color[curr] );
