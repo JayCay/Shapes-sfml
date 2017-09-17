5fd7612c6f4f2fc8c32cb1dc682980fbc2a497ee
Gio Lopez <gener.lopez@gmail.com>
2017-09-17 12:18:06 +0800

    this doesnt work but it's the foundation

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ sf::CircleShape circ[CirSIZE];
 sf::RectangleShape rect[RecSIZE];
 float movement = 20.0 / FPS;
 
+// keybinding
+#define keyUp sf::Keyboard::W
+#define keyDown sf::Keyboard::S
+#define keyLeft sf::Keyboard::A
+#define keyRight sf::Keyboard::D
+#define keyQuit sf::Keyboard::Escape
+
 int colorcycle( int count ) {
 	if ( count >= CSIZE ) {
 		int n = count % CSIZE;
@@ -26,8 +33,9 @@ void threader( sf::RenderWindow* window ) {
 		for ( int n = 0; n < CirSIZE; n++ ) window->draw( circ[n] );
 		for ( int i = 0; i < RecSIZE; i++ ) window->draw( rect[i] );
 		window->display();
-		for ( int i = 0; i < CirSIZE; i++ ) circ[i].move( 0 , movement );
-		for ( int i = 0; i < RecSIZE; i++ ) rect[i].move( movement, 0 );
+		// only moves 2nd to n objects (1 to size)
+		for ( int i = 1; i < CirSIZE; i++ ) circ[i].move( 0 , movement );
+		for ( int i = 1; i < RecSIZE; i++ ) rect[i].move( movement, 0 );
 	}
 }
 
@@ -48,8 +56,12 @@ int main() {
 	int posw = 0;
 	int posh = 0;
 	int curr = 0;
+	bool keyUpPressed = false;
+	bool keyDownPressed = false;
+	bool keyLeftPressed = false;
+	bool keyRightPressed = false;
+	bool keyQuitPressed = false;
 	
-
 	for ( int i = 0; i < CirSIZE; i++ ) {
 		curr = colorcycle( i );
 		posw = rand() % width + 1;
@@ -74,9 +86,54 @@ int main() {
 	while ( window.isOpen() ) {
 		sf::Event event;
 		while ( window.pollEvent( event ) ) {
-			if ( event.type == sf::Event::Closed ) window.close();
+			switch( event.type ) {
+				case sf::Event::KeyPressed:
+					switch( event.key.code ) {
+						case keyUp:
+							keyUpPressed = true;
+							break;
+						case keyDown:
+							keyDownPressed = true;
+							break;
+						case keyLeft:
+							keyLeftPressed = true;
+							break;
+						case keyRight:
+							keyRightPressed = true;
+							break;
+						case keyQuit:
+							keyQuitPressed = true;
+							break;
+					}
+					break;
+				case sf::Event::KeyReleased:
+					switch( event.key.code ) {
+						case keyUp:
+							keyUpPressed = false;
+							break;
+						case keyDown:
+							keyDownPressed = false;
+							break;
+						case keyLeft:
+							keyLeftPressed = false;
+							break;
+						case keyRight:
+							keyRightPressed = false;
+							break;
+						case keyQuit:
+							keyQuitPressed = false;
+							break;
+					}
+					break;
+				case sf::Event::Closed:
+					window.close();
+					break;
+			}
 		}
 	}
+
+	// input/movement code
+	while (keyUpPressed) circ[0].move(0, -200);
 
 	return 0;
 }
