892344c97972358e463a091c4a51a65dbd3be03c
santino60 <31065259+santino60@users.noreply.github.com>
2017-10-12 23:54:37 +0800

    All ball physics
    "they all boing down here" - pennywise

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 #define MASS 1.0f
 #define FRICTION 0.2f * TIMESTEP
 #define ELASTICITY 1.0f
+#define OMASS 0.5f
 
 // keybindings
 #define keyUp sf::Keyboard::W
@@ -35,21 +36,23 @@ float autoMovement = 20.0 / FPS;
 //float getTime = TIMESTEP;
 float force = FORCE;
 float mass = 1.f/MASS;
+float omass = OMASS;
 float friction = FRICTION;
 
-sf::Vector2f cPos( 0.0f, 0.0f );
-sf::Vector2f cVel( 0.0f, 0.f );
-sf::Vector2f cAcel( 0.0f, 0.0f );
-sf::CircleShape circ;
+sf::CircleShape circ[36];
+sf::Vector2f cPos[36];
+sf::Vector2f cVel[36];
+sf::Vector2f cAcel[36];
+
 sf::RenderWindow window( sf::VideoMode( WIDTH, HEIGHT ), "shape me" );
 
 void makeCircles(int x, sf::CircleShape circ[]) {
         int spacex = ORADIUS; 
         int spacey = ORADIUS;
-        for ( int i = 0; i < x; i++ ) {
+        for ( int i = 1; i < x; i++ ) {
             circ[i].setRadius( ORADIUS );
             circ[i].setFillColor( sf::Color::Green );
-            if( i == 0 ){
+            if( i == 1 ){
                 spacex =  spacex + (ORADIUS * 2) + 35;
                 circ[i].setPosition( spacex, spacey );
                 spacex =  spacex + (ORADIUS * 2) + 35;
@@ -68,38 +71,42 @@ void makeCircles(int x, sf::CircleShape circ[]) {
         }
     }
 
-void moveball(){
-    cPos = circ.getPosition();
-    cVel += ( cAcel * TIMESTEP );
-    cPos += ( 0.5f * cAcel * TIMESTEP * TIMESTEP ) + ( cVel * TIMESTEP );
-    if ( frictionMode )cVel -= ( ( friction *cVel ) / mass );
-    circ.setPosition( cPos );
+void moveball( int size){
+    for ( int i = 0; i < size; i++ ) {
+    cPos[i] = circ[i].getPosition();
+    cVel[i] += ( cAcel[i] * TIMESTEP );
+    cPos[i] += ( 0.5f * cAcel[i] * TIMESTEP * TIMESTEP ) + ( cVel[i] * TIMESTEP );
+    if ( frictionMode )cVel[i] -= ( ( friction *cVel[i] ) / mass );
+    circ[i].setPosition( cPos[i] );
+    }
 }
 
-void boing(){
-    cPos = circ.getPosition();
+void boing(int size){
+    for ( int i = 0; i < size; i++ ) {
+    cPos[i] = circ[i].getPosition();
         //elasticity
-        if ( cPos.y  < 0.f + RADIUS ){   
-            cPos.y = 0 + RADIUS;
-            cVel.y = -ELASTICITY * cVel.y;
-            circ.setPosition( cPos );
+        if ( cPos[i].y  < 0.f + RADIUS ){   
+            cPos[i].y = 0 + RADIUS;
+            cVel[i].y = -ELASTICITY * cVel[i].y;
+            circ[i].setPosition( cPos[i] );
             }
-        if (cPos.y > HEIGHT - 1.f - RADIUS){
-            cPos.y = HEIGHT - RADIUS -1.f;
-            cVel.y = -ELASTICITY * cVel.y;
-            circ.setPosition( cPos );
+        if (cPos[i].y > HEIGHT - 1.f - RADIUS){
+            cPos[i].y = HEIGHT - RADIUS -1.f;
+            cVel[i].y = -ELASTICITY * cVel[i].y;
+            circ[i].setPosition( cPos[i] );
         }
-        if ( cPos.x < 0.f + RADIUS ){
-            cPos.x = 0 + RADIUS;
-            cVel.x = -ELASTICITY * cVel.x;
-            circ.setPosition( cPos );
+        if ( cPos[i].x < 0.f + RADIUS ){
+            cPos[i].x = 0 + RADIUS;
+            cVel[i].x = -ELASTICITY * cVel[i].x;
+            circ[i].setPosition( cPos[i] );
         }
 
-        if(cPos.x  > WIDTH -1.f - RADIUS ){
-            cPos.x = WIDTH - RADIUS -1.f;
-            cVel.x = -ELASTICITY * cVel.x;
-            circ.setPosition( cPos );
+        if(cPos[i].x  > WIDTH -1.f - RADIUS ){
+            cPos[i].x = WIDTH - RADIUS -1.f;
+            cVel[i].x = -ELASTICITY * cVel[i].x;
+            circ[i].setPosition( cPos[i] );
         }
+    }
 }
 
 int main( int argc, char *argv[] ) {
@@ -114,7 +121,7 @@ int main( int argc, char *argv[] ) {
     else {
         int x = atoi( argv[1] ); // convert from chararray to int
         // cout << x << endl;
-        if ( x >= 1 && x <= 35 ) size = x;
+        if ( x >= 1 && x <= 35 ) size = x + 1;
         else {
             // if argument out of range
             cout << "USAGE: " << argv[0] << " <int>" << endl;
@@ -127,15 +134,16 @@ int main( int argc, char *argv[] ) {
     window.setActive( false );
     window.setKeyRepeatEnabled(false);
 
+    //sf::CircleShape circ[size];
     // initialise drawing the circle
-    circ.setRadius( RADIUS );
-    circ.setOrigin( RADIUS, RADIUS );
-    circ.setFillColor( sf::Color::Red );
-    circ.setPosition( 50, 50 );
+    circ[0].setRadius( RADIUS );
+    circ[0].setOrigin( RADIUS, RADIUS );
+    circ[0].setFillColor( sf::Color::Red );
+    circ[0].setPosition( 50, 50 );
     
-     sf::CircleShape ocirc[size];
+     
     
-    makeCircles( size, ocirc );
+    makeCircles( size, circ );
 
     while ( window.isOpen() ) {
         // handle input
@@ -143,18 +151,18 @@ int main( int argc, char *argv[] ) {
         while ( window.pollEvent(event) ) {
             if ( event.type == sf::Event::Closed ) window.close();
             if ( event.type == sf::Event::KeyPressed ) {
-                if ( event.key.code == keyUp ) cAcel.y -= (force * mass);
-                if ( event.key.code == keyDown ) cAcel.y +=  force * mass;
-                if ( event.key.code == keyLeft) cAcel.x -= (force * mass);
-                if ( event.key.code == keyRight) cAcel.x += force * mass;
+                if ( event.key.code == keyUp ) cAcel[0].y -= (force * mass);
+                if ( event.key.code == keyDown ) cAcel[0].y +=  force * mass;
+                if ( event.key.code == keyLeft) cAcel[0].x -= (force * mass);
+                if ( event.key.code == keyRight) cAcel[0].x += force * mass;
                 if ( event.key.code == keyClose ) keyClosePressed = true;
                 if ( event.key.code == keyFriction ) keyFrictionPressed = true;
             }
             if ( event.type == sf::Event::KeyReleased ) {
-                if ( event.key.code == keyUp ) cAcel.y = 0;
-                if ( event.key.code == keyDown ) cAcel.y = 0;
-                if ( event.key.code == keyLeft ) cAcel.x = 0;
-                if ( event.key.code == keyRight ) cAcel.x = 0;
+                if ( event.key.code == keyUp ) cAcel[0].y = 0;
+                if ( event.key.code == keyDown ) cAcel[0].y = 0;
+                if ( event.key.code == keyLeft ) cAcel[0].x = 0;
+                if ( event.key.code == keyRight ) cAcel[0].x = 0;
                 if ( event.key.code == keyClose ) keyClosePressed = false;
                 if ( event.key.code == keyFriction ) keyFrictionPressed = false;
             } 
@@ -173,22 +181,22 @@ int main( int argc, char *argv[] ) {
         }
 
         // default colors
-        circ.setFillColor( sf::Color::Red ); 
-        for ( int i = 0; i < size; i++ ) {
-            ocirc[i].setFillColor( sf::Color::Green );
+        circ[0].setFillColor( sf::Color::Red ); 
+        for ( int i = 1; i < size; i++ ) {
+            circ[i].setFillColor( sf::Color::Green );
             }
         // friction mode color
-        if ( frictionMode ) {circ.setFillColor( sf::Color::Blue );
-            for( int i = 0; i < size; i++) ocirc[i].setFillColor( sf::Color::White );
+        if ( frictionMode ) {circ[0].setFillColor( sf::Color::Blue );
+            for( int i = 1; i < size; i++) circ[i].setFillColor( sf::Color::White );
         } 
-        boing(); // ball elasticity function
-        moveball(); // ball movement function
-        cout << cAcel.x << " " << cAcel.y << " " << cVel.x << " " << cVel.y << endl; // DEBUG print data to console
+        boing(size); // ball elasticity function
+        moveball(size); // ball movement function
+        cout << cAcel[0].x << " " << cAcel[0].y << " " << cVel[0].x << " " << cVel[0].y << endl; // DEBUG print data to console
 
         // draw game to screen
         window.clear( sf::Color::Black );
-        window.draw( circ );
-        for( int i = 0; i < size; i++) window.draw( ocirc[i] );
+        window.draw( circ[0] );
+        for( int i = 1; i < size; i++) window.draw( circ[i] );
         window.display();
     }
 
