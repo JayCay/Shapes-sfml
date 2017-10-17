0f970f3917f44b29013aa1735a308a18294dfdb6
santino60 <31065259+santino60@users.noreply.github.com>
2017-10-18 00:18:00 +0800

    AABBs
    +added aabbs
    -doesn't exapand and contract well

--- rect.cpp
+++ rect.cpp
@@ -1,23 +1,45 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
 
 #define FPS 60
 #define RecSIZE 5
 #define WIDTH 800
 #define HEIGHT 600
+#define TIMESTEP 1.f/FPS
 
 using namespace std;
 
 sf::RectangleShape rect[RecSIZE];
+sf::RectangleShape aabb[RecSIZE];
+sf::RectangleShape oaabb[RecSIZE];
+float o_angle[RecSIZE];
+float n_angle[RecSIZE];
+
 float movement = 20.0 / FPS;
 
-void threader( sf::RenderWindow* window ) {
-	while(window->isOpen()) {
-		window->clear( sf::Color::Black );
-		for ( int i = 0; i < RecSIZE; i++ ) window->draw( rect[i] );
-		window->display();
+//void threader( sf::RenderWindow* window ) {
+	//while(window->isOpen()) {
+		
 		//for ( int i = 0; i < RecSIZE; i++ ) rect[i].move( movement, 0 );
-	}
+//	}
+//}
+
+float d2r(float degree){
+	return (degree * M_PI)/180;
+}
+
+void drawaabb(float x, float y, int i, sf::RectangleShape rect[]){
+
+		aabb[i].setSize(sf::Vector2f(x, y));
+		aabb[i].setFillColor(sf::Color::Transparent); 
+		aabb[i].setOutlineColor(sf::Color::Red);
+		aabb[i].setOutlineThickness(1);
+		aabb[i].setOrigin(rect[i].getOrigin());
+		aabb[i].setPosition(rect[i].getPosition());
+	
+
+
 }
 
 int main() {
@@ -39,6 +61,8 @@ int main() {
 	rect[3].setSize( sf::Vector2f( 40.f, 80.f ) );
 	rect[4].setSize( sf::Vector2f( 180.f, 70.f ) );
 	
+
+
 	for ( int i = 0; i < RecSIZE; i++ ) {
 		//posw = rand() % WIDTH + 1;
 		//posh = rand() % HEIGHT + 1;
@@ -48,6 +72,7 @@ int main() {
 		//rect[i].setPosition( 400, 300 );
 		rect[i].setFillColor( sf::Color::Green );
 	}
+
 	
 	rect[0].setPosition( 220 , 125 );
 	rect[1].setPosition( 640, 100 );
@@ -55,14 +80,55 @@ int main() {
 	rect[3].setPosition( 85 , 510 );
 	rect[4].setPosition( 595 , 455 );
 
-	sf::Thread thread( &threader, &window );
-	thread.launch();
+	for (int i = 0;i < RecSIZE;i++){
+		aabb[i].setSize(sf::Vector2f(rect[i].getSize()));
+		oaabb[i].setSize(sf::Vector2f(rect[i].getSize()));
+		aabb[i].setFillColor(sf::Color::Transparent); 
+		aabb[i].setOutlineColor(sf::Color::Red);
+		aabb[i].setOutlineThickness(1);
+		aabb[i].setOrigin( rect[i].getOrigin() );
+		aabb[i].setPosition(rect[i].getPosition());
+	}
+
+	//sf::Thread thread( &threader, &window );
+	//thread.launch();
 
 	while ( window.isOpen() ) {
 		sf::Event event;
 		while ( window.pollEvent( event ) ) {
 			if ( event.type == sf::Event::Closed ) window.close();
 		}
+
+		for ( int i = 0; i <RecSIZE;i++){
+			o_angle[i] = n_angle[i];
+		}
+
+		rect[0].rotate(5 * TIMESTEP);
+		rect[1].rotate(2 * TIMESTEP);
+		rect[2].rotate(3 * TIMESTEP);
+		rect[3].rotate(10 * TIMESTEP);
+		rect[4].rotate(7 * TIMESTEP);
+
+		n_angle[0] +=  5 * TIMESTEP;
+		n_angle[1] +=  2 * TIMESTEP;
+		n_angle[2] +=  3 * TIMESTEP;
+		n_angle[3] += 10 * TIMESTEP;
+		n_angle[4] += 7 * TIMESTEP;
+
+		for ( int i = 0; i < RecSIZE; i++ ){
+		float x_o = oaabb[i].getSize().x;
+		float y_o = oaabb[i].getSize().y;		
+		float x_n = (x_o * std::cos((n_angle[i]* M_PI/180))) - (y_o * std::sin((n_angle[i]* M_PI/180)));
+		float y_n = (y_o * std::cos((n_angle[i]* M_PI/180))) + (x_o * std::sin((n_angle[i]* M_PI/180)));
+		drawaabb(x_n,y_n, i, rect);
+		}
+
+		window.clear( sf::Color::Black );
+		for ( int i = 0; i < RecSIZE; i++ ){ window.draw( rect[i] );
+		window.draw( aabb[i] );
+		}
+		window.display();
+
 	}
 
 	return 0;
